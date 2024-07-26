4dcee7c4e59cac93176e01559322fd53b7fc3d58
Moses Ndungu <moseswambui044@gmail.com>
2024-07-27 00:26:42 +0300

    Adding First Test Case

--- chatapp/chat/tests.py
+++ chatapp/chat/tests.py
@@ -1,3 +1,30 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Room, Message
 
-# Create your tests here.
+
+class MessageModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="12345")
+        self.room = Room.objects.create(name="Test Room", description="This is a test Room")
+
+    def test_create_message(self):
+        message = Message.objects.create(
+            room=self.room,
+            user=self.user,
+            content="This is a Test Message"
+        )
+        self.assertEqual(message.room, self.room)
+        self.assertEqual(message.user, self.user)
+        self.assertEqual(message.content, "This is a Test Message")
+    
+"""
+    def test_message_str(self):
+        message = Message.objects.create(
+            room=self.room,
+            user=self.user,
+            content="This is another test message"
+        )
+        self.assertEqual(str(message), "This is another test")
+"""
