daa9a6d2a62fb8f6a9475846ccb65598606d1db6
Moses Ndungu <moseswambui044@gmail.com>
2024-07-27 01:37:53 +0300

    Configured Postgresql DB in settings file

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -99,8 +99,11 @@ WSGI_APPLICATION = 'chatapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_BOST"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS")
     }
 }
 
