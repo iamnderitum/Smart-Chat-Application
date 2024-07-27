e91b2e88b6fdcd965149be79385b1d8d9fd1ebb9
Moses Ndungu <moseswambui044@gmail.com>
2024-07-27 23:05:03 +0300

    Created Custom Django User model. Created & Migrated project migrations

--- chatapp/authentication/models.py
+++ chatapp/authentication/models.py
@@ -1,3 +1,39 @@
+"""
+Custom User DB Models
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from base.models import BaseModel
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_set",
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_set",
+        blank=True
+    )
--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -165,3 +165,5 @@ STATIC_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "authentication.User"
