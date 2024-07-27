41da959212b19211e5b1626f8ebbd7eb32a3700c
Moses Ndungu <moseswambui044@gmail.com>
2024-07-28 01:49:17 +0300

    User Management Implementation -> Updating user. Mock tests for user management Implementation

--- chatapp/authentication/urls.py
+++ chatapp/authentication/urls.py
@@ -9,4 +9,5 @@ app_name = "authentication"
 urlpatterns = [
     path("create/", views.CreateUserView.as_view(), name="create"),
     path("token/", views.CreateTokenView.as_view(), name="token"),
+    path("me/", views.ManageUserView.as_view(), name="me"),
 ]
--- chatapp/authentication/views.py
+++ chatapp/authentication/views.py
@@ -1,7 +1,7 @@
 """
 Views for the user API.
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework.permissions import AllowAny
@@ -24,3 +24,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated User."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the Authenticated user."""
+        return self.request.user
