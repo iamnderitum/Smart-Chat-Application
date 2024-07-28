09c3e1d7b049dfff022d8b2c76d7e5fad3faf2f9
Moses Ndungu <moseswambui044@gmail.com>
2024-07-28 04:18:20 +0300

    Setup Django Admin: Source code and tests to List, Modify & Create users

--- chatapp/authentication/admin.py
+++ chatapp/authentication/admin.py
@@ -1,10 +1,43 @@
+"""
+Django admin customization
+"""
 from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from authentication.models import User
 
-from .models import User
 
-
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    class Meta:
-        model = User
-
+    """Define the Admin pages for users."""
+    ordering = ["id",]
     list_display = ("email", "name", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissins"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            }
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "name",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            )
+        }),
+    )
